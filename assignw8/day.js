1c0829c08d8bca945af768ab596622ae13345149
Patrick Lowman <plowman@gmail.com>
2012-04-02 02:06:06 -0400

    added local storage

--- assignw8/day.js
+++ assignw8/day.js
@@ -1,85 +1,43 @@
 (function(window) {
-	
-	
-	
-	
-	
-	/**
-	 * Main driver object 
-	 * @param {Object} options
-	 * 
-	 * 
-	 * @config renderTo String selector
-	 */
-	window.myDay = function(options) {
-		var config = options || {};
-		
-		var renderTo = config.renderTo || window.document.body;
-		
-		var beginTheDay = function() {
-			$(renderTo).html("Today is...<div id='addRoom'></div>");
-			this.addRoom.beginRoomAdding();
-		};
-		
-		/**
-		 * As a user I want to add a room. A room has a description. Once added put it 
-		 * on the page
-		 */
-		var AddRoom = function(){
-			var state = {
-				addButtonClicked: false,
-				rooms: [
-				{description:"Room 1"},
-				{description:"Room 2"}
-				]	
-			};
-			
-			this.promptForRoom = function(){
-				var roomHtml = "";
-				roomHtml += "Add Description:<input name='roomDesc' type='text'></input>url for room image:<input name='url' type='text'></input><button class='room-addbutton'>Add Room</button>";
-				$("#addRoom").html($("#addRoom").html()+roomHtml);
-			};
-			
-			this.renderRooms = function(){
-				var roomHtml = "";
-				for(room in state.rooms){
-					roomHtml += '<div class="room">'+state.rooms[room].description+'</div>';
-				}
-				$("#addRoom").html(roomHtml);
-			}
-			
-			this.beginRoomAdding = function() {
-				//loop here
-				this.renderRooms();
-				this.promptForRoom();
-			}
-			
-		}
-			
-		return  {
-			beginTheDay: beginTheDay,
-			addRoom: new AddRoom()
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	$(window.document).ready(function(){
-		var day = myDay({
-			renderTo: ".addrooms"
-		});
-		
-		day.beginTheDay();
-	}) 
-	
 
-	
-	
+	var rooms = localStorage && localStorage["roomsDesc"] ? JSON
+			.parse(localStorage["roomsDesc"]) : {
+		list : []
+	};
+	var html = "";
+
+	function renderRooms() {
+
+		html = "";
+		if (localStorage && JSON) {
+			localStorage["roomsDesc"] = JSON.stringify(rooms);
+		}
+		var html = '<div class="rooms-building-floor">';
+		for ( var count = 0; count < rooms.list.length; ++count) {
+			var currDesc = rooms.list[count].desc;
+			if (count % 3 == 0 && count != 0) {
+				html += '<div class="rooms-building-floor-number">'
+						+ Math.ceil(count / 3)
+						+ '</div></div><div class="rooms-building-floor">'
+			}
+
+			html += '<div class="rooms-building-floor-room">' + currDesc
+					+ '</div>';
+		}
+		html += '<div class="rooms-building-floor-number">'
+				+ Math.ceil(count / 3) + '</div></div>';
+		$('.rooms-building').html(html);
+	}
+
+	$(window.document).ready(function() {
+		renderRooms();
+		$(".rooms-header-addbutton").click(function() {
+			var desc = $('.rooms-header-add-desc')[0].value
+			rooms.list.push({
+				desc : desc
+			});
+			renderRooms();
+		})
+	});
+
 })(window);
